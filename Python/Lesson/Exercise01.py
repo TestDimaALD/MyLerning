fcd4cf3c8c99d599369c973a3f5d8eb6f42cf1c8
TestDimaALD <Dmitry.PUGIN@aldautomotive.ru>
2023-10-06 18:57:57 +0300

    one

---
+++ Python/Lesson/Exercise01.py
@@ -0,0 +1,23 @@
+print ('Test1')
+print ('Test2')
+print('1', '2', '4', '8', '16')
+
+print('Какой хороший день!')
+print()
+print('Работать мне не лень!')
+
+print ('4 8 15 16 23 42')
+print (4,8,15,16,23,42)
+
+print('Как тебя зовут?')
+name = input()
+print('Привет,', name)
+
+name = input()
+print('Привет, ', name)
+
+name = input()
+print(name, '- чемпион!')
+
+print ('Привет,',input(), end='!')
+
